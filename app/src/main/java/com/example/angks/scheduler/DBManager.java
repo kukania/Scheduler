75a1905811ad860690b36cc88d9513e47f131404
kukania <angkswnstn@naver.com>
2016-03-29 13:13:32 +0900

    commit

--- app/src/main/java/com/example/angks/scheduler/DBManager.java
+++ app/src/main/java/com/example/angks/scheduler/DBManager.java
@@ -2,6 +2,7 @@ package com.example.angks.scheduler;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQuery;
@@ -12,12 +13,23 @@ import android.util.Log;
  */
 public class DBManager extends SQLiteOpenHelper {
     public static int id;
+    public SQLiteDatabase myDb;
     public DBManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
         super(context,name,factory,version);
+
+    }
+    public void setID(){
+        SQLiteDatabase db = getReadableDatabase();
+        String query="select id from TODO order by id desc limit 0,1;";
+        Cursor cursor=db.rawQuery(query,null);
+        id=cursor.getInt(0);
+        Log.d("SETID",id+"");
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
+        myDb=db;
         db.execSQL("create table TODO(id integer primary key autoincrement, title varchar(32) not null, startTime varchar(32) not null, endTime varchar(32),importantPoint integer not null,pushAlarm integer, myHour integer, myMinute integer, color integer, typeNum integer);");
+        Log.d("test","test");
         db.execSQL("create table JOB(jobId integer primary key autoincrement,id integer, DDayCheck integer,location varchar(32),content varchar(32),who varchar(32),alarmCustom integer)");
         db.execSQL("create table MEMORIAL(memorialId integer primary Key autoincrement, id integer, illustration integer, DDayCheck integer,content varchar(32));");
         db.execSQL("create table PERSON(id integer primary key autoincrement, name varchar(16), phone varchar(16), memorialId integer);");
